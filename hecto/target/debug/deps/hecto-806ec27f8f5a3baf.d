0f36c5a95c60ff49b047f29d14edd3ffbf7f3268
maldr0w <mvrtinstave@gmail.com>
2023-08-04 16:43:13 +0200

    x

---
+++ hecto/target/debug/deps/hecto-806ec27f8f5a3baf.d
@@ -0,0 +1,5 @@
+/home/maldr0w/rust/hecto/target/debug/deps/hecto-806ec27f8f5a3baf: src/main.rs
+
+/home/maldr0w/rust/hecto/target/debug/deps/hecto-806ec27f8f5a3baf.d: src/main.rs
+
+src/main.rs:
---
+++ hecto/target/debug/hecto.d
@@ -0,0 +1,1 @@
+/home/maldr0w/rust/hecto/target/debug/hecto: /home/maldr0w/rust/hecto/src/main.rs
